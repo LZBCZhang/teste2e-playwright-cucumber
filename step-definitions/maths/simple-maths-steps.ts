d80ff83e260262940572d3f91b90edd16b70f840
lizha <li.zhang1113@gmail.com>
2023-01-31 23:48:40 +0100

    clean

--- step-definitions/maths/simple-maths-steps.ts
+++ step-definitions/maths/simple-maths-steps.ts
@@ -2,19 +2,18 @@ import { CustomWorld } from '../../world';
 import { Before, Given, Then, When } from '@cucumber/cucumber';
 import expect from 'expect';
 
-Given('I have a simple maths calculator', async function (this: CustomWorld) {
-});
+Given('I have a simple maths calculator', async function (this: CustomWorld) {});
 
 Given('a variable is set to {int}', async function (this: CustomWorld, value: number) {
-  expect("").toBe(value);
+  expect('').toBe(value);
 });
 
 When('I increment this variable by {int}', async function (this: CustomWorld, value: number) {
-  expect("").toBe(value);
+  expect('').toBe(value);
 });
 
 Then('the variable should contain {int}', async function (this: CustomWorld, value: number) {
-  expect("").toBe(value);
+  expect('').toBe(value);
 });
 
 /**
